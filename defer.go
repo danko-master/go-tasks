01de0a9520a88057af91e8f0b61f7190836fd66b
Daniil <danko.master>
2026-08-19 22:51:46 +0300

    Defer tasks

--- defer.go
+++ defer.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // Что напечатает программа?
 
 // 1 - Оценивается выражение в инструкции return и записывается в возвращаемую ячейку.
@@ -35,6 +37,96 @@ func f2() (x int) {
 }
 
 func main() {
-	println(f1())
-	println(f2())
+	// println(f1())
+	// println(f2())
+	// dtask1()
+
+	// deferEvaluationAsUsual()
+	// deferEvaluationOnDemand()
+
+	// fmt.Println(deferChangeReturnValue().value)
+
+	// Последовательность вывода defer
+	// 	defer func() {
+	// 		fmt.Println("defer 2")
+	// 	}()
+	// 	F()
+	// // defer 1
+	// // 	defer 2
+	// // 		panic: panic
+	// goroutine 1 [running]:
+	// main.F()
+	// exit status 2
+
+	// "Контролируемая паника"
+	protect(func() {
+		panic("Test panic\n")
+	})
+	fmt.Println("All done")
+}
+
+func protect(g func()) {
+	defer func() {
+		fmt.Println("done")
+
+		// Перехват паники
+		// Если паники не было, возвращает nil
+		if x := recover(); x != nil {
+			// Если паника произошла, перехватывает переданный в panic аргумент.
+			fmt.Printf("run time panic: %v", x)
+		}
+	}()
+
+	fmt.Println("start")
+	g()
+}
+
+func F() {
+	defer func() {
+		fmt.Println("defer 1")
+	}()
+
+	panic("panic")
+}
+
+// В какой последовательности отобразятся цифры
+// 3-2-1
+func dtask1() {
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	defer fmt.Println(3)
+}
+
+type data struct {
+	value int
+}
+
+func deferEvaluationAsUsual() {
+	d := data{}
+	// 0
+	// Аргументы функции для defer вычисляются в момент объявления, т.е. 0
+	defer fmt.Println(d.value)
+
+	d.value++
+}
+
+func deferEvaluationOnDemand() {
+	d := data{}
+	// Чтобы значение было то, которое будет в конце основной функции, т.е. 1
+	defer func() {
+		// 1
+		fmt.Println(d.value)
+	}()
+
+	d.value++
+}
+
+func deferChangeReturnValue() (d data) {
+	defer func() {
+		// Будет возвращено 43
+		d.value = 43
+	}()
+
+	d.value = 42
+	return
 }
